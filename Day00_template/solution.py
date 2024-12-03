0bee2627c8f897a2c1464d778321b23e751a1a92
Wojciech Jazienicki <wojtekjaz@gmail.com>
2024-12-03 19:21:39 +0000

    adding template for each day

---
+++ Day00_template/solution.py
@@ -0,0 +1,22 @@
+from utils.input_handler import no_linebreaks
+from utils.performance import timer
+
+part1_path = "Day00/input.txt"
+part2_path= "Day00/input2.txt"
+
+@timer
+def part1(part1_path):
+    pass
+    # return result
+
+@timer
+def part2(part2_path):
+    pass
+    # return result
+
+
+if __name__=="__main__":
+    print(part1(part1_path))
+    print(part2(part2_path))
+
+ 
